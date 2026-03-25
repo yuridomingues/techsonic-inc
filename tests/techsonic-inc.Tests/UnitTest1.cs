fcd77bd461834f05aa2bd5073174511a044c9539
unknown <carlosotoline@gmail.com>
2026-03-25 12:51:40 -0300

    adicionando a parte da giulia

---
+++ tests/techsonic-inc.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace techsonic_inc.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Deve_retornar_verdadeiro()
+    {
+        // Arrange
+        var resultado = true;
+
+        // Act
+        var valorObtido = resultado;
+
+        // Assert
+        Assert.True(valorObtido);
+    }
+}
